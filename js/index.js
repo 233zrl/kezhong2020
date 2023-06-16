f597be04a2dde1ba12094c150695e1f66e10d7b9
233zrl <108232664+233zrl@users.noreply.github.com>
2023-06-17 05:48:32 +0800

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,28 @@
+function bingdu () {
+    alert("警告！！该“病毒”没有病毒。");
+const dialog = document.getElementById("bingdu1");
+dialog.showModal();
+} 
+function bingdu2 () {
+    const dialog = document.getElementById("bingdu2");
+    dialog.showModal();
+}
+function bingdu3 () {
+    const dialog = document.getElementById("bingdu3");
+    dialog.showModal();
+}
+
+function dialog1 () {
+    const dialog = document.getElementById("dialog1");
+    dialog.showModal();
+}
+function dialog2 () {
+    const dialog = document.getElementById("dialog2");
+    dialog.showModal();
+}
+function dialog3 () {
+
+    const dialog = document.getElementById("dialog3");
+
+    dialog.showModal();
+}
